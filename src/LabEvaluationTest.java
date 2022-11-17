1b1192130d45827f54483b6df29d7f7b70062edb
Sushama Devi <sushamamaurya44@gmail.com>
2022-11-17 12:06:13 +0530

    some changes in file

--- src/LabEvaluationTest.java
+++ src/LabEvaluationTest.java
@@ -3,5 +3,10 @@ public class LabEvaluationTest {
         System.out.println("welcome to CDAC NOIDA");
         System.out.println("welcome to CDAC NOIDA");
         System.out.println("Changes added to local");
+        int c = sum(10,10);
+        System.out.println("Sum : "+ c);
+    }
+    public static int sum(int a, int b){
+        return a + b;
     }
 }
